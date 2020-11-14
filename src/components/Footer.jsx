1c996e8bda14031d8191f3e58d8e510915bc0a43
Lucas Grönborg <lucas94gronborg@gmail.com>
2020-11-14 19:03:54 +0100

    Created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Circle from "../components/Circle";
+
+const StyledFooter = styled.footer`
+  position: relative;
+  overflow: hidden;
+  height: 500px;
+  padding-top: 48px;
+`;
+
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  a {
+    font-size: 16px;
+    font-family: var(--font-heading);
+    color: var(--primary-color);
+    text-decoration: none;
+    margin-bottom: 16px;
+  }
+`;
+
+export default function Footer({ children }) {
+  return (
+    <StyledFooter>
+      <Circle color="var(--secondary-color)" size="1000px" left="50%" top="0" />
+      <Circle color="var(--primary-color)" size="50px" left="20%" top="25%" />
+      <Circle color="var(--primary-color)" size="350px" left="100%" top="25%" />
+      <Content>{children}</Content>
+    </StyledFooter>
+  );
+}
