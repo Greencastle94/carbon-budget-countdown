cc1afa422f73df207428e38b6d3ca4ddcd484cfe
Lucas Grönborg <lucas94gronborg@gmail.com>
2020-11-14 19:02:33 +0100

    Created circle component

---
+++ src/components/Circle.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const StyledCircle = styled.div`
+  height: ${props => props.size};
+  width: ${props => props.size};
+  border-radius: 50%;
+  background-color: ${props => props.color};
+
+  position: absolute;
+  transform: translateX(-50%);
+  left: ${props => props.left};
+  top: ${props => props.top};
+`;
+
+export default function Circle({ color, size, left, top }) {
+  return <StyledCircle color={color} size={size} left={left} top={top} />;
+}
