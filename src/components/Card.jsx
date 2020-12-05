26039bccff2c06357f00647cd13a545c2f8101e5
Lucas Grönborg <lucas94gronborg@gmail.com>
2020-12-05 19:44:07 +0100

    Update Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,85 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { VictoryBar } from "victory";
+import { average } from "../utils/utils";
+import trendArrowUp from "../../static/arrow-trend-up.svg";
 
 const StyledCard = styled.div`
   margin-bottom: 16px;
   padding: 16px;
+  background-color: white;
   border-radius: 4px;
   box-shadow: 0 2px 8px 0 rgba(99, 99, 99, 0.2);
+
+  .row {
+    display: flex;
+  }
+
+  .column {
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 `;
 
-export default function Card({ children }) {
+const BAR_WIDTH = 20;
+
+export default function Card({
+  children,
+  data = [
+    { year: 1, value: "1,0" },
+    { year: 2, value: "3,0" },
+    { year: 3, value: "2,0" },
+    { year: 4, value: "1,0" },
+    { year: 5, value: "2,0" },
+  ],
+  unit,
+}) {
+  const processedData = data.map(el => ({
+    year: el.year,
+    value: Number(el.value.replace(",", ".").replace("−", "-")),
+  }));
+
+  const timePeriod = 30;
+  const valueChange = (
+    average(processedData.slice(processedData.length - timePeriod), "value") -
+    average(processedData.slice(0, timePeriod), "value")
+  ).toFixed(1);
+
   return (
-    <StyledCard >
-      {children}
+    <StyledCard>
+      <h3>{children}</h3>
+      <div className="row">
+        <div className="column">
+          <p>{`${valueChange > 0 ? "+" : ""} ${valueChange} ${unit}`}</p>
+          <img src={trendArrowUp} alt="" />
+        </div>
+        <VictoryBar
+          data={processedData}
+          x="year"
+          y="value"
+          barWidth={BAR_WIDTH}
+          height={150}
+          padding={{
+            top: 0,
+            right: BAR_WIDTH / 2,
+            bottom: 0,
+            left: BAR_WIDTH / 2,
+          }}
+          cornerRadius={{
+            topLeft: BAR_WIDTH / 2,
+            topRight: BAR_WIDTH / 2,
+            bottomLeft: BAR_WIDTH / 2,
+            bottomRight: BAR_WIDTH / 2,
+          }}
+          style={{
+            data: {
+              fill: "var(--primary-color)",
+            },
+          }}
+        />
+      </div>
     </StyledCard>
   );
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,3 +11,11 @@ export function scrollIntoView(id) {
 export function getMilliSecondsLeftOfBudget(currentBudget, currentEmissions) {
   return (currentBudget / currentEmissions) * 31556952000;
 }
+
+export function average(array, property) {
+  let valueSum = 0;
+  for (let el in array) {
+    valueSum += array[el][property];
+  }
+  return valueSum / array.length;
+}
