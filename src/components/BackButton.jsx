9826e0c62b3e69862f7264c435a197a252f49700
Lucas Grönborg <lucas94gronborg@gmail.com>
2020-12-13 20:33:52 +0100

    Add BackButton component

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Link } from "gatsby";
+import arrowImg from "../../static/arrow-right.svg";
+
+const StyledBackButton = styled(Link)`
+  display: flex;
+  margin: 2rem 16px 0;
+  max-width: 1140px;
+  font-family: var(--font-heading);
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  img {
+    transform: rotate(180deg);
+    margin-right: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    margin: 30px auto;
+    margin-bottom: 0;
+  }
+`;
+
+export default function BackButton({ to }) {
+  return (
+    <StyledBackButton to={to}>
+      <img src={arrowImg} alt="" />
+      <b>Tillbaka</b>
+    </StyledBackButton>
+  );
+}
