c38582146ea44f38c7941583f44aeebe54b1e31e
Lucas Grönborg <lucas94gronborg@gmail.com>
2020-12-13 20:35:41 +0100

    Add BackButton to cookie-policy page

--- src/pages/cookie-policy.jsx
+++ src/pages/cookie-policy.jsx
@@ -1,38 +1,12 @@
 import React from "react";
-import styled from "@emotion/styled";
-import { Link } from "gatsby";
 import Layout from "../components/Layout";
+import BackButton from "../components/BackButton";
 import ContentWrapper from "../components/ContentWrapper";
-import arrowImg from "../../static/arrow-right.svg";
-
-const BackLink = styled(Link)`
-  display: flex;
-  margin: 2rem 16px 0;
-  max-width: 1140px;
-  font-family: var(--font-heading);
-
-  &:hover {
-    text-decoration: underline;
-  }
-
-  img {
-    transform: rotate(180deg);
-    margin-right: 1rem;
-  }
-
-  @media (min-width: 768px) {
-    margin: 30px auto;
-    margin-bottom: 0;
-  }
-`;
 
 export default function CookiePolicy() {
   return (
     <Layout>
-      <BackLink to="/">
-        <img src={arrowImg} alt="" />
-        <b>Tillbaka</b>
-      </BackLink>
+      <BackButton to="/" />
       <ContentWrapper>
         <h2>Vad är Cookies?</h2>
         <p style={{ margin: "0" }}>
@@ -44,8 +18,7 @@ export default function CookiePolicy() {
           och internetuppkoppling, såsom operativsystem, webbläsarversion och
           IP-adress.
         </p>
-      </ContentWrapper>
-      <ContentWrapper>
+
         <h2>Vad används den här information till?</h2>
         <p>
           Information om hur många som besöker hemsidan, hur länge man besöker,
