70962a1087fd7f07b46e21f7a3a1677fc7b6775e
Lucas Grönborg <lucas94gronborg@gmail.com>
2020-12-20 20:52:40 +0100

    Add section elements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ const CURRENT_EMISSIONS = YEARLY_EMISSIONS[CURRENT_YEAR];
 const START_DATE = new Date("Jan 1, 2020 00:00:00");
 
 export default function Home() {
-  const StartContent = styled.div`
+  const StartSection = styled.section`
     position: relative;
     overflow: hidden;
     height: calc(100vh - 44px);
@@ -35,7 +35,7 @@ export default function Home() {
 
   return (
     <Layout>
-      <StartContent>
+      <StartSection>
         <Circle
           color="var(--primary-color)"
           size={`${50}px`}
@@ -57,95 +57,97 @@ export default function Home() {
           Tid kvar tills Sveriges koldioxidbudget är slut.
         </CountdownText>
         <ScrollDownArrow />
-      </StartContent>
+      </StartSection>
 
-      <ContentWrapper>
-        <EmissionReductions
-          currentYear={CURRENT_YEAR}
-          yearlyEmissions={YEARLY_EMISSIONS}
-        />
-      </ContentWrapper>
+      <section>
+        <ContentWrapper>
+          <EmissionReductions
+            currentYear={CURRENT_YEAR}
+            yearlyEmissions={YEARLY_EMISSIONS}
+          />
+        </ContentWrapper>
 
-      <ContentWrapper dark center>
-        <h2>Vad händer när tiden är slut?</h2>
-        <p style={{ margin: "0" }}>
-          Inget kommer plötsligt att hända om tiden tar slut men Sverige har då
-          misslyckats att fullfölja sitt åtagande till Paris-avtalet. Vi börjar
-          då att gnaga på andra länders koldioxidbudgetar.
-        </p>
-      </ContentWrapper>
+        <ContentWrapper dark center>
+          <h2>Vad händer när tiden är slut?</h2>
+          <p style={{ margin: "0" }}>
+            Inget kommer plötsligt att hända om tiden tar slut men Sverige har
+            då misslyckats att fullfölja sitt åtagande till Paris-avtalet. Vi
+            börjar då att gnaga på andra länders koldioxidbudgetar.
+          </p>
+        </ContentWrapper>
 
-      <ContentWrapper center>
-        <h2>Hur räknar vi ut tiden?</h2>
-        <p style={{ margin: "0" }}>
-          Vi har utgått från totalen av hur mycket Sverige har kvar att släppa
-          ut innan vi misslyckats med vårt åtagande till Paris-avtalet. Sedan
-          dragit av motsvarande vad Sverige släppte ut senaste året med
-          tillgänglig data.
-        </p>
+        <ContentWrapper center>
+          <h2>Hur räknar vi ut tiden?</h2>
+          <p style={{ margin: "0" }}>
+            Vi har utgått från totalen av hur mycket Sverige har kvar att släppa
+            ut innan vi misslyckats med vårt åtagande till Paris-avtalet. Sedan
+            dragit av motsvarande vad Sverige släppte ut senaste året med
+            tillgänglig data.
+          </p>
 
-        <BudgetCharts
-          currentEmissions={CURRENT_EMISSIONS}
-          currentYear={CURRENT_YEAR}
-          currentBudget={CURRENT_BUDGET}
-        />
-        <Timeline
-          startDate={START_DATE}
-          currentEmissions={CURRENT_EMISSIONS}
-          currentYear={new Date().getFullYear()}
-          currentBudget={CURRENT_BUDGET}
-        />
-      </ContentWrapper>
+          <BudgetCharts
+            currentEmissions={CURRENT_EMISSIONS}
+            currentYear={CURRENT_YEAR}
+            currentBudget={CURRENT_BUDGET}
+          />
+          <Timeline
+            startDate={START_DATE}
+            currentEmissions={CURRENT_EMISSIONS}
+            currentYear={new Date().getFullYear()}
+            currentBudget={CURRENT_BUDGET}
+          />
+        </ContentWrapper>
 
-      <ContentWrapper dark center>
-        <h2>Vi kan stoppa klockan</h2>
-        <p style={{ margin: "0" }}>
-          Genom att minska våra utsläpp kan vi förlänga tiden vi har på oss, och
-          tillslut, stoppa klockan innan tiden tagit slut.
-        </p>
-      </ContentWrapper>
+        <ContentWrapper dark center>
+          <h2>Vi kan stoppa klockan</h2>
+          <p style={{ margin: "0" }}>
+            Genom att minska våra utsläpp kan vi förlänga tiden vi har på oss,
+            och tillslut, stoppa klockan innan tiden tagit slut.
+          </p>
+        </ContentWrapper>
 
-      <ContentWrapper>
-        <Circle
-          color="var(--primary-color)"
-          size={`${450}px`}
-          left={`${100}%`}
-          top={`${32}%`}
-        />
-        <h2>Klimateffekterna i Sverige</h2>
-        <p>
-          Konsekvenserna är redan märkbara, och för att få en helhetsbild över
-          vart vi är påväg kan man titta på trender över flera indikatorer.
-        </p>
-        <CardContainer>
-          <Card data={averageYearTempData} unit="°C">
-            Medeltemperatur
-          </Card>
-          <Card data={seaLevelRiseData} unit="cm">
-            Havsnivå
-          </Card>
-          <Card unit="dagar">Antal snödagar</Card>
-        </CardContainer>
-      </ContentWrapper>
+        <ContentWrapper>
+          <Circle
+            color="var(--primary-color)"
+            size={`${450}px`}
+            left={`${100}%`}
+            top={`${32}%`}
+          />
+          <h2>Klimateffekterna i Sverige</h2>
+          <p>
+            Konsekvenserna är redan märkbara, och för att få en helhetsbild över
+            vart vi är påväg kan man titta på trender över flera indikatorer.
+          </p>
+          <CardContainer>
+            <Card data={averageYearTempData} unit="°C">
+              Medeltemperatur
+            </Card>
+            <Card data={seaLevelRiseData} unit="cm">
+              Havsnivå
+            </Card>
+            <Card unit="dagar">Antal snödagar</Card>
+          </CardContainer>
+        </ContentWrapper>
 
-      <ContentWrapper>
-        <h2>Är det inte kört redan?</h2>
-        <p style={{ margin: "0" }}>
-          Visst kan det kännas så ibland… Alternativet att inte göra allt för
-          att lösa det skulle däremot vara så förördande att det inte kan vara
-          ett alternativt. Du kan påverka mer än vad du tror. Från att minska
-          dina egna utsläpp till att påverka andra och förändra normer.
-        </p>
-      </ContentWrapper>
+        <ContentWrapper>
+          <h2>Är det inte kört redan?</h2>
+          <p style={{ margin: "0" }}>
+            Visst kan det kännas så ibland… Alternativet att inte göra allt för
+            att lösa det skulle däremot vara så förördande att det inte kan vara
+            ett alternativt. Du kan påverka mer än vad du tror. Från att minska
+            dina egna utsläpp till att påverka andra och förändra normer.
+          </p>
+        </ContentWrapper>
 
-      <ContentWrapper center style={{ marginBottom: "30px" }}>
-        <h2>Håll konversationen vid liv!</h2>
-        <p>
-          Vi kan bara göra skillnad om vi förstår utmaningen, och den korta
-          begränsade tiden vi har på oss.
-        </p>
-        <Button>Dela sidan</Button>
-      </ContentWrapper>
+        <ContentWrapper center style={{ marginBottom: "30px" }}>
+          <h2>Håll konversationen vid liv!</h2>
+          <p>
+            Vi kan bara göra skillnad om vi förstår utmaningen, och den korta
+            begränsade tiden vi har på oss.
+          </p>
+          <Button>Dela sidan</Button>
+        </ContentWrapper>
+      </section>
     </Layout>
   );
 }
