d80253947848c76941daffca4243825121ff117f
Lucas Grönborg <lucas94gronborg@gmail.com>
2021-05-13 16:54:41 +0200

    Add preliminary data for 2020

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ export const ENV_STAGE = "stage";
 
 export const START_BUDGET = 370; // (MtCO2) post-2019
 export const BUDGET_START_DATE = new Date("Jan 1, 2020 00:00:00");
-export const YEARLY_EMISSIONS = { "2019": 50.9, "2018": 52.2 }; // MtCO2
-export const CURRENT_DATA_YEAR = 2019;
+export const YEARLY_EMISSIONS = { "2020": 47.4, "2019": 50.9, "2018": 52.2 }; // MtCO2
+export const CURRENT_DATA_YEAR = 2020;
 export const CURRENT_EMISSIONS = YEARLY_EMISSIONS[CURRENT_DATA_YEAR];
 export const NECESSARY_REDUCTION_RATE = -12;
 
