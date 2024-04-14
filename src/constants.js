32eaa2cd3d46fb50eaf625caa62218ce2cc62391
Lucas Grönborg <lucas94gronborg@gmail.com>
2024-04-14 20:44:34 +0200

    update yearly emissions

--- src/constants.js
+++ src/constants.js
@@ -3,11 +3,11 @@ export const ENV_STAGE = "stage";
 export const START_BUDGET = 370; // (MtCO2) post-2019
 export const BUDGET_START_DATE = new Date("Jan 1, 2020 00:00:00");
 export const YEARLY_EMISSIONS = {
-  2022: 45.22,
-  2021: 47.77,
-  2020: 46.21,
-  2019: 50.60,
-  2018: 51.78,
+  2022: 45.25,
+  2021: 47.72,
+  2020: 46,
+  2019: 50.2,
+  2018: 51.56,
 }; // MtCO2
 export const CURRENT_DATA_YEAR = 2022;
 export const CURRENT_EMISSIONS = YEARLY_EMISSIONS[CURRENT_DATA_YEAR];
